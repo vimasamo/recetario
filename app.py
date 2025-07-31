13a571a3b078295631e3b2ebfeb8c1c101706880
Victor Manuel <victorm.sanchezm@gmail.com>
2025-07-30 22:07:42 -0600

    Recetario

---
+++ app.py
@@ -0,0 +1,9 @@
+# app.py
+
+import streamlit as st
+
+st.set_page_config(page_title="Recetariu TM6", layout="centered")
+
+st.title("🍰 Bienvenido a mi minisitio web")
+st.write("Este es un ejemplo simple con **Streamlit** y Python 3.")
+st.success("¡Funciona correctamente!")
